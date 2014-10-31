c45fe4dddcf5d0c6a701274a086968e85453b2ee
Xiaochen Xu <xiaochen-xu@uiowa.edu>
2014-10-31 17:46:39 -0500

    create pages for admin

---
+++ admin/details.aspx.vb
@@ -0,0 +1,21 @@
+﻿
+Partial Class details
+    Inherits System.Web.UI.Page
+
+    Protected Sub FormView1_ItemDeleted(sender As Object, e As FormViewDeletedEventArgs) Handles FormView1.ItemDeleted
+
+        Dim deletedname As String = e.Values("name").ToString()
+        lbl_deletedname.text = deletedname & " has been deletd from the databse "
+        Response.AddHeader("REFRESH", "3;URL=./default.aspx")
+
+    End Sub
+
+    Protected Sub FormView1_ItemUpdated(sender As Object, e As FormViewUpdatedEventArgs) Handles FormView1.ItemUpdated
+        Response.AddHeader("REPRESH", "3;URL=./default.aspx")
+    End Sub
+
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        FormView1.Focus()
+    End Sub
+End Class
---
+++ admin/insert.aspx.vb
@@ -0,0 +1,12 @@
+﻿
+Partial Class css_insert
+    Inherits System.Web.UI.Page
+
+    Protected Sub FormView1_ItemInserted(sender As Object, e As FormViewInsertedEventArgs) Handles FormView1.ItemInserted
+        Response.Redirect("./default.aspx")
+    End Sub
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        FormView1.Focus()
+    End Sub
+End Class
